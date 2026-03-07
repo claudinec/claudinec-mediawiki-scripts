f238b9d2dfee73604c054d805cb0fb5568c1b6d9
User:ClaudineChionh <hello+wikimedia@claudinec.net>
2026-03-07 15:07:02 +1100

    update copyPageToClipboard licence: GPL-3.0-or-later OR CC-BY-SA-4.0

--- src/copyPageToClipboard.js
+++ src/copyPageToClipboard.js
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2021 User:ClaudineChionh
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later OR CC-BY-SA-4.0
 /**
- * [[User:ClaudineChionh/Scripts/copyPageToClipboard.js]]
  * Copy the page content to the system clipboard.
  * This script uses the Clipboard API – for browser compatibility see
  * https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
@@ -26,6 +25,6 @@ $(document).ready(function () {
         var copyLink = mw.util.addPortletLink("p-tb", "#", "Copy page content", "t-copypage", "Copy page content to clipboard");
         $(copyLink).click(function (event) {
             copyPageToClipboard();
-        })
+        });
     }
 });
